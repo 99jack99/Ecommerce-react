a1bdfc012ef85808fc66d66ec79082d2f310c076
jackson <jackson55adl@gmail.com>
2022-09-14 12:11:58 +0200

    styling and designing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,18 +4,23 @@ import "./Header.scss";
 
 const Header = () => {
   return (
-    
     <div className="nav">
-        <div className="navBox">
-            <div className="navLogo">Z E N</div>
-            <div className="navItems">
-                <div className="navItem">Log In</div>
-                <div className="navItem">Sign up</div>
-                <div className="navItem">
-                  [cart]
-                </div>
-            </div>
+      <div className="navBox">
+        <div className="navLogo">
+          <img id="logoImg" src="../../../public/logo/zenlogo-b.png" alt="retard" />
         </div>
+        <div className="navItems">
+          <div className="navItem">Log In</div>
+          <div className="navItem">Sign up</div>
+          <div className="navItem cartItem">
+            <img
+              id="cartIcon"
+              src="../../../public/icons/bag.png"
+              alt="Cart bag"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
